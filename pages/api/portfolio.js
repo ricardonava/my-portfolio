3324b90aa9c2d3b875fc5cd0b681eeee6c1dd603
Ricardo <ricardo.a.nava@gmail.com>
2020-07-28 20:00:38 -0700

    Add pages

---
+++ pages/api/portfolio.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function portfolio() {
+  return <h1>portfolio.js</h1>;
+}
---
+++ pages/api/resume.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function resume() {
+  return <h1>resume.js</h1>;
+}
